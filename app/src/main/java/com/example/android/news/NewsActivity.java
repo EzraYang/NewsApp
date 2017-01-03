356f95378542a2288c145405becc4d69ffbf0b9f
EzraYang <deleguo@gmail.com>
2017-01-03 21:17:55 +0800

    take in user's topic

--- app/src/main/java/com/example/android/news/NewsActivity.java
+++ app/src/main/java/com/example/android/news/NewsActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,16 +17,22 @@ import java.util.List;
 
 public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
 
-    private String mFakeUrl = "http://content.guardianapis.com/search?q=programmer&api-key=test";
+    private String mFakeUrlString = "http://content.guardianapis.com/search?q=programmer&api-key=test";
+
+    private String mBaseUrlString = "http://content.guardianapis.com/search?api-key=test";
 
     private NewsAdapter mAdapter;
 
     private ListView mListView;
 
+    private EditText mTopicField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        mTopicField = (EditText) findViewById(R.id.topic_field) ;
 
         mListView = (ListView) findViewById(R.id.list);
         mAdapter = new NewsAdapter(NewsActivity.this, new ArrayList<News>());
@@ -42,14 +50,29 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
-        getLoaderManager().initLoader(0, null, this);
+        ImageView searchBtn = (ImageView) findViewById(R.id.search_btn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLoaderManager().initLoader(1, null, NewsActivity.this);
+            }
+        });
+
 
     }
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
 
-        return new NewsLoader(this, mFakeUrl);
+        String topic = mTopicField.getText().toString();
+
+        Uri baseUrl = Uri.parse(mBaseUrlString);
+        Uri.Builder urlBuilder = baseUrl.buildUpon();
+        urlBuilder.appendQueryParameter("q", topic);
+
+        String queryUrl = urlBuilder.toString();
+
+        return new NewsLoader(this, queryUrl);
     }
 
     @Override
