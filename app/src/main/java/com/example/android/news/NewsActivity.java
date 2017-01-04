416c618095e5b2d716970f6edcf26dc8eb37ce38
EzraYang <deleguo@gmail.com>
2017-01-04 10:47:59 +0800

    fix null listview prob

--- app/src/main/java/com/example/android/news/NewsActivity.java
+++ app/src/main/java/com/example/android/news/NewsActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.news;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,16 +31,23 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private EditText mTopicField;
 
+    private int loaderId = 0;
+
+    private String mTopic = "";
+
+    private TextView mEmptyTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        mTopicField = (EditText) findViewById(R.id.topic_field) ;
-
         mListView = (ListView) findViewById(R.id.list);
         mAdapter = new NewsAdapter(NewsActivity.this, new ArrayList<News>());
         mListView.setAdapter(mAdapter);
+
+        mEmptyTextView = (TextView) findViewById(R.id.text_when_empty);
+        mListView.setEmptyView(mEmptyTextView);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -54,21 +65,49 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getLoaderManager().initLoader(1, null, NewsActivity.this);
+                // clear up mAdapter leads to clear listView
+                mAdapter.clear();
+                // set mEmptyTextView an empty String
+                // in case it was showing no_key_word or no_conn warning previously
+                mEmptyTextView.setText("");
+
+                // check the internet connection on the device
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()){
+                    // find out what user's key search word
+                    mTopicField = (EditText) findViewById(R.id.topic_field) ;
+
+                    // check if user has enter a key word
+                    if (!mTopicField.getText().toString().isEmpty() && mTopicField.getText().toString() != mTopic){
+
+                        mTopic = mTopicField.getText().toString();
+
+                        // if topic changes,
+                        // init a new loader with loaderId increment 1
+                        mAdapter.clear();
+                        loaderId += 1;
+
+                        getLoaderManager().initLoader(loaderId, null, NewsActivity.this);
+
+                    } else {
+                        mEmptyTextView.setText(R.string.new_topic_needed);
+                    }
+                } else {
+                    mEmptyTextView.setText(R.string.no_conn);
+                }
             }
         });
-
-
     }
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
 
-        String topic = mTopicField.getText().toString();
+        mTopic = mTopicField.getText().toString();
 
         Uri baseUrl = Uri.parse(mBaseUrlString);
         Uri.Builder urlBuilder = baseUrl.buildUpon();
-        urlBuilder.appendQueryParameter("q", topic);
+        urlBuilder.appendQueryParameter("q", mTopic);
 
         String queryUrl = urlBuilder.toString();
 
@@ -82,7 +121,7 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
-        mAdapter.clear();
+
 
         if ( data != null & !data.isEmpty()){
             mAdapter.addAll(data);
